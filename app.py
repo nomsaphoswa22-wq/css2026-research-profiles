b7cb00e3b35e2989392e3346cff0d9af70a63ac7
nomsaphoswa22-wq <nomsaphoswa22@gmail.com>
2026-02-02 21:41:16 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Research Profile | Nomsa Phoswa",
+    page_icon="🧬",
+    layout="centered"
+)
+
+st.title("Nomsa Phoswa")
+st.write("### MSc Biochemistry Researcher")
+st.write("Sefako Makgatho Health Sciences University")
+
+st.divider()
+
+st.header("About Me")
+st.write("""
+I am an MSc Biochemistry student with a research focus on nontuberculous mycobacteria (NTM)
+and the metabolic effects of anti-tuberculosis drugs.
+""")
+
+st.header("Research Title")
+st.write("""
+**Investigation of the effects of bedaquiline and clofazimine on the metabolic profile of
+nontuberculous mycobacteria**
+""")
+
+st.header("Research Interests")
+st.markdown("""
+- Nontuberculous Mycobacteria (NTM)
+- Tuberculosis and Drug Resistance
+- Metabolomics
+- GC-MS Analysis
+""")
+
+st.header("Supervisors")
+st.markdown("""
+- Dr N. A. Makhado  
+- Mr M. Nemukula  
+- Dr V. S. Thibane  
+""")
+
+st.header("Contact")
+st.markdown("""
+📞 079 275 7954  
+📧 nomsaphoswa22@gmail.com  
+""")
